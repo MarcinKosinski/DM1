890fb14ed8cc9044979be9b68f667138db06f627
sommermarta <sommerm@student.mini.pw.edu.pl>
2014-05-17 15:36:54 +0200

    lda+qda

--- p.R
+++ p.R
@@ -4,8 +4,20 @@ library("rpart")
 library("klaR")
 
 se <- read.arff("http://archive.ics.uci.edu/ml/machine-learning-databases/00266/seismic-bumps.arff")
-se <- se[,-c(14:16)]
-head(se)
+se <- se[,-c(14:16,18,9)]
+head(se,2)
+dim(se)
+
+# specjalnie dla qda, dane przerabiam z faktorowych na ilosciowe:
+
+se_r <- se
+levels(se_r$seismic) <- c(0,1,2,3)
+levels(se_r$seismoacoustic) <- c(0,1,2,3)
+levels(se_r$shift) <- c(0,1)
+levels(se_r$ghazard) <- c(0,1,2,3)
+se_r[,c(1,2,3,8)] <- apply(se_r[,c(1,2,3,8)],2,as.numeric)
+
+head(se_r,2)
 
 # podzial na probe testowa i treningowa:
 
@@ -15,22 +27,39 @@ s <- sample(1:dim(se)[1],ile)
 tren <- se[s,]
 test <- se[-s,]
 
-# bedziemy budowac model na treningowej, a testowac na testowej :D
+tren_r <- se_r[s,]
+test_r <- se_r[-s,]
 
-# model buduje bez zmiennych maxenergy i nbumps, bo sa skorelowane z innymi
-# i bez zmiennych nbumps6, 7 i 89, bo byly cale zerowe
+# bedziemy budowac model na treningowej, a testowac na testowej :D
 
 # lda
 
-mod_lda <- lda(class ~ seismic +seismoacoustic +shift +genergy +gpuls +gdenergy +
-               gdpuls +ghazard +nbumps2 +nbumps3 +nbumps4 +nbumps5 +energy,
-               data=tren)
+mod_lda <- lda(class~., data=tren)
 pred <- predict(mod_lda, newdata=test)$class
 pred
-table(pred)
+t <- table(pred,test$class)
+100*sum(diag(t))/sum(t)
+t
+table(test$class)
 
+czulosc <- t[2,2]/(sum(t[2,]))
+czulosc
+precyzja <- t[2,2]/sum(t[,2])
+precyzja
 
+# czyli raczej slabo :(
 
+# qda
 
+mod_qda <- qda(class~., data=tren_r)
 
+pred <- predict(mod_qda, newdata=test_r)$class
+t <- table(pred,test_r$class)
+100*sum(diag(t))/sum(t)
+t
+
+czulosc <- t[2,2]/(sum(t[2,]))
+czulosc
+precyzja <- t[2,2]/sum(t[,2])
+precyzja
 
